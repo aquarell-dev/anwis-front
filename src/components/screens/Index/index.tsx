f22525e46d85ca34cc1988163ec7340be4dfcb22
Aquarell <aquarell.dev@gmail.com>
2022-10-19 22:08:09 +0300

    Home page

---
+++ src/components/screens/Index/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+
+import { AbsoluteCenteredContainer } from '../../ui/Container';
+
+import AuthForm from './AuthForm';
+
+import useAuth from '../../../hooks/useAuth';
+
+
+const Index: FC = () => {
+  const { isAuth, user } = useAuth();
+
+  return (
+    <AbsoluteCenteredContainer>
+      {isAuth ? (
+        <h1 className='text-4xl text-center'>
+          Добро пожаловать, {user}.
+        </h1>
+      ) : (
+        <>
+          <h1 className='text-3xl text-center font-medium mb-4'>Авторизация</h1>
+          <AuthForm/>
+        </>
+      )}
+    </AbsoluteCenteredContainer>
+  );
+};
+
+export default Index;
