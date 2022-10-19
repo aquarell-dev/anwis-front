c7d09ccc90f6ccd0b3d2d2959cf2ee872b5516a3
Aquarell <aquarell.dev@gmail.com>
2022-10-19 22:08:46 +0300

    wrapped into router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Layout from './components/layouts/Layout';
+
+import Index from './components/screens/Index';
+import Settings from './components/screens/Settings';
+import China from './components/screens/China';
 
 function App() {
   return (
-    <p>anwis</p>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Index />} />
+
+            <Route path={'/settings'} element={<Settings />} />
+
+            <Route path={'/china'} element={<China />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
