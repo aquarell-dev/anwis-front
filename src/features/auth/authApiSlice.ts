0866b42c65ccbf5af7f0db3cb4145e96a70758c2
Aquarell <aquarell.dev@gmail.com>
2022-10-19 22:06:56 +0300

    authorization slice with api

---
+++ src/features/auth/authApiSlice.ts
@@ -0,0 +1,16 @@
+import { apiSlice } from '../../store/api/apiSlice';
+import { IUserCredentials } from '../../components/screens/Index/index.types';
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: build => ({
+    login: build.mutation<void, IUserCredentials>({
+      query: credentials => ({
+        url: '/api/token/',
+        method: 'POST',
+        body: { ...credentials }
+      })
+    }),
+  })
+});
+
+export const { useLoginMutation } = authApiSlice;
