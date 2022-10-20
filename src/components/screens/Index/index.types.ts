0882de6c08a31a0950daec573b35730092a7c576
Aquarell <aquarell.dev@gmail.com>
2022-10-20 15:30:41 +0300

    refactoring

--- src/components/screens/Index/index.types.ts
+++ src/components/screens/Index/index.types.ts
@@ -1,21 +1,3 @@
-import { UseFormRegister } from 'react-hook-form';
-
-import React from 'react';
-
-export interface IInput {
-  type?: React.HTMLInputTypeAttribute;
-  value: string;
-  handler: React.ChangeEventHandler<HTMLInputElement>;
-  placeholder: string;
-  error?: boolean;
-}
-
-export interface IRHFInput extends Pick<IInput, 'placeholder' | 'type'> {
-  label: string;
-  register: UseFormRegister<any>; // TODO fix typing here
-  required: boolean | string;
-}
-
 export interface IUserCredentials {
   username: string;
   password: string;
