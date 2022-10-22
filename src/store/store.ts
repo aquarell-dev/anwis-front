cda97370c332d849671e26c0fc6b88f990980844
Aquarell <aquarell.dev@gmail.com>
2022-10-22 14:35:43 +0300

    add new api to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { authReducer } from '../features/auth/authSlice';
 import { apiSlice } from './api/apiSlice';
+import { orderApi } from '../features/order/orderApi';
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    [orderApi.reducerPath]: orderApi.reducer,
     auth: authReducer,
   },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware).concat(orderApi.middleware),
   devTools: true
 });
 
