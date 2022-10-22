42791ab8d4e5a695af76d719a87e1502459e4281
Aquarell <aquarell.dev@gmail.com>
2022-10-22 14:38:14 +0300

    refactoring

---
+++ src/components/screens/China/components/ChooseCreateBlock/index.tsx
@@ -0,0 +1,49 @@
+import { FC, PropsWithChildren } from 'react';
+import { SetState } from '../../../../../utils/types';
+
+interface IProps<T> extends PropsWithChildren {
+  setState: SetState<boolean>;
+}
+
+const ChoseCreateBlock: FC<IProps<boolean>> = ({ children, setState }) => {
+  return (
+    <>
+      <div className='flex items-center w-full justify-between space-x-4'>
+        {children}
+        <div className='flex items-center'>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 cursor-pointer text-emerald-600"
+            onClick={() => setState(true)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 cursor-pointer text-emerald-600"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ChoseCreateBlock;
