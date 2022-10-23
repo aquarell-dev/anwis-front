0b6673dd8cdad68b82491ffacab956251735addb
Aquarell <aquarell.dev@gmail.com>
2022-10-23 23:16:52 +0300

    products

--- src/components/screens/China/types.ts
+++ src/components/screens/China/types.ts
@@ -1,9 +1,21 @@
-import { UseFormRegister } from 'react-hook-form';
-import { IChinaDistributor, IIndividualEntrepreneur, IOrderForProject } from '../../../features/order/types';
+import { FieldErrors, UseFormRegister } from 'react-hook-form';
+import { IChinaDistributor, IIndividualEntrepreneur, IOrderForProject, IStatus } from '../../../features/order/types';
 
 export interface IFormControls {
   register: UseFormRegister<any>;
   individualEntrepreneurs: IIndividualEntrepreneur[];
   orderForProjects: IOrderForProject[];
   chinaDistributors: IChinaDistributor[];
+  statuses: IStatus[];
+  errors: FieldErrors<IOrderForm>;
+}
+
+export interface IOrderForm {
+  status: string;
+  individual_entrepreneur: string;
+  china_distributor: string;
+  order_for_project: string;
+  products: number[];
+  draft: boolean;
+  comment: string;
 }
