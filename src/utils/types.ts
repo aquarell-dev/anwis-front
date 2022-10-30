42afe87ad83fe9c46f27456ef01c7dfbf824ec30
Aquarell <aquarell.dev@gmail.com>
2022-10-31 01:36:09 +0300

    added global types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,23 @@
 import { Dispatch, SetStateAction } from 'react';
 
+import { MutationTrigger } from '@reduxjs/toolkit/dist/query/react/buildHooks';
+
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  FetchBaseQueryMeta,
+  MutationDefinition
+} from '@reduxjs/toolkit/query';
+
 export type SetState<T> = Dispatch<SetStateAction<T>>;
 
 export type AnyObject = {
   [k: string]: any;
 };
+
+export type Mutation<T> = MutationTrigger<MutationDefinition<
+  T,
+  BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError, {}, FetchBaseQueryMeta>,
+  any,
+  void>>;
