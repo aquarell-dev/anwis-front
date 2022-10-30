aaa216031db1d7600584e414641e9967d0032e66
Aquarell <aquarell.dev@gmail.com>
2022-10-31 01:40:23 +0300

    comps for order form

---
+++ src/components/screens/China/components/OrderForm/Notification.js
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+
+import Expand from 'react-expand-animated';
+
+import moment from 'moment';
+
+import { Controller } from 'react-hook-form';
+
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
+import { IndigoButton } from '../../../../ui/Button';
+import { Input } from '../../../../ui/Input';
+
+import { useCreateTaskMutation, useListTasksQuery } from '../../../../../features/order/orderApi';
+
+import { notifyError, notifySuccess } from '../../../../../utils/notify';
+
+
+const animatedComponents = makeAnimated();
+
+// had to use js cause were unable to find types for the expand lib
+
+const Notification = ({order, control}) => {
+  const [notification, setNotification] = useState('');
+  const [taskCreateOpen, setTaskCreateOpen] = useState(false);
+  const [dateTime, setDateTime] = useState(moment(new Date('00:00 01/01/2023')).format('YYYY-MM-DDThh:mm'));
+  const [hasMenuBeenOpened, setMenuBeenOpened] = useState(false);
+
+  const [createTask, {isLoading}] = useCreateTaskMutation();
+
+  const {data, error} = useListTasksQuery(null);
+
+  if (error) return <p>Error...</p>;
+
+  if (!data) return <p>Loading</p>;
+
+  const options = data.map(d => ({label: `${d.task}(${d.datetime})`, value: d.id}));
+  const defaultValue = order && order.tasks.map(task => ({label: `${task.task}(${task.datetime})`, value: task.id}));
+
+  return (
+    <>
+      {data && (
+        <div className='z-[40] flex flex-col space-y-4'>
+          <div className="flex items-center space-x-4">
+            <div className="w-[550px]">
+              <Controller
+                control={control}
+                name={'tasks'}
+                render={({field: {onChange, ref, value}}) => (
+                  <Select
+                    ref={ref}
+                    closeMenuOnSelect={false}
+                    components={animatedComponents}
+                    placeholder={'Задачи'}
+                    isMulti
+                    onMenuOpen={() => setMenuBeenOpened(true)}
+                    value={hasMenuBeenOpened ? options.find((c) => c.value === value) : defaultValue}
+                    onChange={(val) => onChange(val.map((c) => c.value))}
+                    options={options}
+                  />
+                )}
+              />
+            </div>
+            <IndigoButton
+              type={'button'}
+              text={taskCreateOpen ? 'Закрыть' : 'Новая задача'}
+              customWidth={'w-60'}
+              handler={() => setTaskCreateOpen(prev => !prev)}
+            />
+          </div>
+          <Expand open={taskCreateOpen}>
+            <div className="flex items-center space-x-4">
+              <div className="w-60">
+                <Input
+                  value={notification}
+                  handler={e => setNotification(e.target.value)}
+                  placeholder={'Новая задача...'}
+                />
+              </div>
+              <LocalizationProvider dateAdapter={AdapterMoment}>
+                <DateTimePicker
+                  className='w-96'
+                  renderInput={(props) => <TextField {...props} />}
+                  label="Дата и время"
+                  value={dateTime}
+                  onChange={(newValue) => {
+                    setDateTime(newValue);
+                  }}
+                  inputFormat={'hh:mm DD/MM/yyyy'}
+                />
+              </LocalizationProvider>
+              <IndigoButton
+                type={'button'}
+                text={'Создать'}
+                customWidth={'w-60'}
+                handler={() => {
+                  setTaskCreateOpen(false);
+                  createTask({task: notification, datetime: dateTime})
+                    .unwrap()
+                    .then(() => notifySuccess('Задача успешно создана'))
+                    .catch(() => notifyError('Задача не создана'));
+                }}
+              />
+            </div>
+          </Expand>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Notification;
---
+++ src/components/ui/SlideAlert/index.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { FC } from 'react';
+
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide
+    direction="up"
+    ref={ref} {...props} />;
+});
+
+
+const SlideAlert: FC<{
+  title: string,
+  content: string,
+  buttonText: string[],
+  onClose: () => void,
+  onDeny: () => void,
+  onAccept: () => void,
+  open: boolean
+}> = ({ title, content, open, buttonText, onAccept, onClose, onDeny }) => {
+  const [continueButton, denyButton] = buttonText;
+
+  return (
+    <Dialog
+      open={open}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={onClose}
+      aria-describedby="alert-dialog"
+    >
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog">
+          {content}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onDeny}>{denyButton}</Button>
+        <Button onClick={onAccept}>{continueButton}</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default SlideAlert;
