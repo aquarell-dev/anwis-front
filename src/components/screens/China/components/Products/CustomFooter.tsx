2261a238a84868c47c00cf2e849b2c22a70b4a25
Aquarell <aquarell.dev@gmail.com>
2022-10-31 12:24:09 +0300

    order info in footer

---
+++ src/components/screens/China/components/Products/CustomFooter.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { FC } from 'react';
+import Box from '@mui/material/Box';
+import { IOrder } from '../../../../../features/order/types';
+
+const CustomFooter: FC<{
+  order: IOrder | undefined
+}> = ({ order }) => {
+  return (
+    <>
+      {order && (
+        <Box className='p-4 flex items-center space-x-8'>
+          <p>Сумма ¥: {order.total_cny}</p>
+          <p>Сумма ₽: {order.total_rub}</p>
+          <p>Сумма доп. затрат ₽: {order.total_expenses}</p>
+          <p>Количество товаров: {order.total_quantity}</p>
+        </Box>
+      )
+      }
+    </>
+  );
+};
+
+export default CustomFooter;
