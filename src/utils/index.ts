af448ea85907c53dbfb453eb9ced0bffc0601e9b
Aquarell <aquarell.dev@gmail.com>
2022-11-02 20:29:25 +0300

    get date difference in days

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,5 @@
 export const cn = (...classes: string[]) => classes.filter(Boolean).join(' ');
+
+export const getDateDiff = ({ biggerDate, smallerDate }: { biggerDate: Date, smallerDate: Date }): number => Math.ceil(
+  (biggerDate.getTime() - smallerDate.getTime()) / (1000 * 3600 * 24)
+);
