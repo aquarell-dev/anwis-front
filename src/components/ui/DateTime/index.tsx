0ca0e6cbf9cc860168b56c7adfd48a0048e9425b
Aquarell <aquarell.dev@gmail.com>
2022-11-04 02:00:02 +0300

    custom date picker component

---
+++ src/components/ui/DateTime/index.tsx
@@ -0,0 +1,31 @@
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker as MuiDatePicker } from '@mui/x-date-pickers/DatePicker';
+import { FC } from 'react';
+import moment from 'moment';
+import { SetState } from '../../../utils/types';
+
+type TDateValue = moment.Moment;
+
+type DatePickerProps = {
+  label: string;
+  value: TDateValue,
+  setValue: SetState<TDateValue>,
+  customOnChange?: (value: (TDateValue | null), keyboardInputValue?: (string | undefined)) => void;
+};
+
+export const CustomDatePicker: FC<DatePickerProps> = ({ label, value, setValue, customOnChange }) => {
+  return (
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+      <MuiDatePicker
+        label={label}
+        openTo="day"
+        views={['year', 'month', 'day']}
+        value={value}
+        inputFormat={'DD/MM/YYYY'}
+        onChange={customOnChange ? (value) => customOnChange(value) : (value) => value && setValue(value)}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>);
+};
