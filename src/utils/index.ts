7596e9381fbd334561490297660e774e61b80001
Aquarell <aquarell.dev@gmail.com>
2022-11-04 01:59:21 +0300

    method to convert from ru to us format

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,8 @@
 export const cn = (...classes: string[]) => classes.filter(Boolean).join(' ');
 
-export const getDateDiff = ({ biggerDate, smallerDate }: { biggerDate: Date, smallerDate: Date }): number => Math.ceil(
+export const getDateDiff = (biggerDate: Date, smallerDate: Date): number => Math.ceil(
   (biggerDate.getTime() - smallerDate.getTime()) / (1000 * 3600 * 24)
 );
+
+export const convertDateToUSFormat = (dateString: string) =>
+  new Date(dateString.replace(/(\d+[/])(\d+[/])/, '$2$1'));
