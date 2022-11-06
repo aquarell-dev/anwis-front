048b641c3a7ff457982f881fb1fd83529f0373ba
Aquarell <aquarell.dev@gmail.com>
2022-11-07 00:28:47 +0300

    header in its component

---
+++ src/components/screens/China/Order/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+import { getFourDigitId } from '../../../../../../utils';
+
+import { IOrder } from '../../../../../../features/order/order.types';
+
+const Header: FC<{ order?: IOrder }> = ({ order }) => {
+  const date = order ? order.date : new Date().toLocaleTimeString('en-GB') + ' ' + new Date().toLocaleDateString('en-GB');
+
+  return (
+    <h1 className='text-3xl mb-3'>
+      Заказ {getFourDigitId(order?.id)} поставщику
+      в <span className='underline text-indigo-600'>Китай</span> от {date}
+    </h1>
+  );
+};
+
+export default Header;
