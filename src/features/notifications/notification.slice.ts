056e18e61c2dbe15ffb0fa0ca2c6027db4a22f78
Aquarell <aquarell.dev@gmail.com>
2022-11-07 00:25:40 +0300

    added slice for notifications and ip

---
+++ src/features/notifications/notification.slice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Notification, NotificationInitialState } from './notification.types';
+
+const initialState: NotificationInitialState = {
+  notifications: []
+};
+
+const notificationSlice = createSlice({
+  name: 'notifications',
+  initialState,
+  reducers: {
+    createNotification: (state, action: PayloadAction<Notification>) => {
+      state.notifications = [...state.notifications, action.payload];
+    },
+    clearNotifications: (state => {
+      state.notifications = [];
+    })
+  }
+});
+
+export const notificationActions = notificationSlice.actions;
+
+export const notificationReducer = notificationSlice.reducer;
