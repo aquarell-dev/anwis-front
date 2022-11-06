cb85dd13e0ac2033eda9e0a53b625072c2675078
Aquarell <aquarell.dev@gmail.com>
2022-11-07 00:26:52 +0300

    added ip select

---
+++ src/components/ui/Navbar/components/Individual/index.tsx
@@ -0,0 +1,36 @@
+import { FC, useRef } from 'react';
+import { SetState } from '../../../../../utils/types';
+import { useOutside } from '../../../../../hooks/useOutside';
+import { useTypedSelector } from '../../../../../hooks/useTypedSelector';
+import { useActions } from '../../../../../hooks/useActions';
+
+const Individual: FC<{ open: boolean, setOpen: SetState<boolean> }> = ({ open, setOpen }) => {
+  const ref = useRef(null);
+  useOutside(ref, () => setOpen(false), 'individualException');
+
+  const { individuals } = useTypedSelector(store => store.individual);
+  const { selectIndividual } = useActions();
+
+  return (
+    <>
+      {open && (
+        <div
+          ref={ref}
+          className='absolute top-16 w-36 text-black bg-white shadow-lg'
+        >
+          {individuals.map(individual => (
+            <p
+              key={individual.id}
+              onClick={() => { selectIndividual(individual); setOpen(false) }}
+              className='py-1 px-2 hover:bg-gray-100 cursor-pointer transition duration-300 ease-in-out'
+            >
+              {individual.individual_entrepreneur}
+            </p>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Individual;
