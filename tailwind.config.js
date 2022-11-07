c9c8e5d0459b3c9502965d4957f75ba860c8c97e
aquarell <aquarell.dev@gmail.com>
2022-11-07 20:44:31 +0300

    tw conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        usm: "320px",
+        sm: "425px",
+        md: "768px",
+        lg: "1024px",
+        sxl: "1440px",
+        xl: "1920px",
+      },
+    },
   },
   plugins: [],
 };
