6bfb8a0da1a6e083b4e156621cd7f53f702c6996
Aquarell <aquarell.dev@gmail.com>
2022-11-08 21:51:09 +0300

    added isLoading

--- src/hooks/useUploadDocument.ts
+++ src/hooks/useUploadDocument.ts
@@ -3,13 +3,18 @@ import { AxiosResponse } from 'axios';
 import { TDocument } from '../features/documents/document.types';
 import { useActions } from './useActions';
 import useNotifications from './useNotifications';
+import { useState } from 'react';
 
 const useUploadDocument = (type: 'document' | 'photo') => {
   const { newDocument } = useActions();
   const { notifyError } = useNotifications();
 
+  const [isLoading, setLoading] = useState(false);
+
   const create = (file: File): Promise<AxiosResponse<TDocument, any>> => {
     const formData = new FormData();
+
+    setLoading(true);
 
     formData.append(type, file, file.name);
 
@@ -17,11 +22,11 @@ const useUploadDocument = (type: 'document' | 'photo') => {
       type + '/',
       formData
     )
-      .then(res => newDocument(res.data))
-      .catch(err => { notifyError('Картинка не добавлена'); return err; } );
+      .then(res => { newDocument(res.data); setLoading(false); })
+      .catch(err => { notifyError('Картинка не добавлена'); setLoading(false); return err; } );
   };
 
-  return { create };
+  return { create, isLoading };
 };
 
 export default useUploadDocument;
