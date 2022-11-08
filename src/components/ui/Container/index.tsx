5173ce01479c3c367e4fa39a74c47f73ec01877c
Aquarell <aquarell.dev@gmail.com>
2022-11-08 21:56:07 +0300

    container

--- src/components/ui/Container/index.tsx
+++ src/components/ui/Container/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-export const SidebarContainer: FC<{ children: React.ReactNode }> = ({ children }) => <div className='ml-20 mr-8'>{children}</div>;
+export const ContentContainer: FC<{ children: React.ReactNode }> = ({ children }) => <div className='mx-4 my-8'>{children}</div>;
 
 export const AbsoluteCenteredContainer: FC<{ children: React.ReactNode }> = ({ children }) =>
   <div className='absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2'>{children}</div>;
