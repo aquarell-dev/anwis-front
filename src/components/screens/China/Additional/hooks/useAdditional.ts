da570575aa0ae1e2ec9cbc81f26a39611a35a9af
Aquarell <aquarell.dev@gmail.com>
2022-11-08 21:53:41 +0300

    new hook for prjects and distributors

---
+++ src/components/screens/China/Additional/hooks/useAdditional.ts
@@ -0,0 +1,21 @@
+import { useListChinaDistributorsQuery } from '../../../../../store/api/distributor.api';
+import { useListOrderForProjectsQuery } from '../../../../../store/api/orderForProject.api';
+import { useState } from 'react';
+
+const useAdditional = () => {
+  const { data: distributors, isLoading: distributorsLoading, error: distributorsError } = useListChinaDistributorsQuery(null);
+  const { data: projects, isLoading: projectsLoading, error: projectsError } = useListOrderForProjectsQuery(null);
+
+  const [changeValue, setChangeValue] = useState('');
+  const [createValue, setCreateValue] = useState('');
+
+  return {
+    projects, distributors,
+    isLoading: distributorsLoading || projectsLoading,
+    error: distributorsError || projectsError,
+    value: changeValue, setValue: setChangeValue,
+    createValue, setCreateValue
+  }
+};
+
+export default useAdditional;
