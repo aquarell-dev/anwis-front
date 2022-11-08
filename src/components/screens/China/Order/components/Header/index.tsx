c427042990041064491c039381e4b94e9e1c06c8
Aquarell <aquarell.dev@gmail.com>
2022-11-08 21:55:58 +0300

    usememo

--- src/components/screens/China/Order/components/Header/index.tsx
+++ src/components/screens/China/Order/components/Header/index.tsx
@@ -1,11 +1,14 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 
 import { getFourDigitId } from '../../../../../../utils';
 
 import { IOrder } from '../../../../../../features/order/order.types';
 
 const Header: FC<{ order?: IOrder }> = ({ order }) => {
-  const date = order ? order.date : new Date().toLocaleTimeString('en-GB') + ' ' + new Date().toLocaleDateString('en-GB');
+  const date = useMemo(
+    () => order ? order.date : new Date().toLocaleTimeString('en-GB') + ' ' + new Date().toLocaleDateString('en-GB'),
+    []
+  );
 
   return (
     <h1 className='text-3xl mb-3'>
