d7c14695ae2d85014f6ecaaf141e0cd98d05e2b1
aquarell <aquarell.dev@gmail.com>
2022-11-16 17:08:02 +0300

    changed types

--- src/components/screens/Acceptance/types.ts
+++ src/components/screens/Acceptance/types.ts
@@ -1,22 +1,29 @@
-import { GridColDef } from '@mui/x-data-grid';
+import { GridColDef } from '@mui/x-data-grid'
+import { AcceptanceProduct } from './../../../types/acceptance.types'
 
-import { IProduct, IProductSpecs } from '../../../features/order/order.types';
-import { Acceptance, AcceptanceProduct, StaffMember } from '../../../types/acceptance.types';
+import { IProduct, IProductSpecs } from '../../../features/order/order.types'
+import {
+  Acceptance,
+  AcceptanceProductSpecification,
+  StaffMember
+} from '../../../types/acceptance.types'
 
 export type Row = Pick<
   Acceptance,
   'id' | 'title' | 'cargo_number' | 'cargo_volume' | 'cargo_weight' | 'created_at'
->;
+>
 
-export type Columns = GridColDef & { field: keyof Row | 'redirect' };
+export type Columns = GridColDef & { field: keyof Row | 'redirect' }
 
-export type AcceptanceProductColumn = GridColDef & { field: keyof IProduct | keyof IProductSpecs };
+export type AcceptanceProductColumn = GridColDef & {
+  field: keyof AcceptanceProduct | keyof AcceptanceProductSpecification
+}
 
 export type AcceptanceProductRow = Omit<IProductSpecs, 'product' | 'cny_to_rub_course'> &
-  Omit<IProduct, 'photo_id' | 'url' | 'category' | 'color'>;
+  Omit<IProduct, 'photo_id' | 'url' | 'category' | 'color'>
 
-export type StaffMemberFields = Pick<StaffMember, 'username' | 'password'>;
+export type StaffMemberFields = Pick<StaffMember, 'username' | 'password'>
 
-export type RussianProductColumn = GridColDef & { field: keyof AcceptanceProduct };
+export type RussianProductColumn = GridColDef & { field: keyof AcceptanceProduct }
 
-export type RussianProductRow = Pick<AcceptanceProduct, 'id' | 'article' | 'brand' | 'color'>;
+export type RussianProductRow = Pick<AcceptanceProduct, 'id' | 'article' | 'brand' | 'color'>
