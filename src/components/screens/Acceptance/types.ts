76bddaa7c275b91fc39e0cc68de1be724c737945
Aquarell <aquarell.dev@gmail.com>
2022-11-21 12:22:32 +0300

    new acceptance types

--- src/components/screens/Acceptance/types.ts
+++ src/components/screens/Acceptance/types.ts
@@ -1,12 +1,12 @@
 import { GridColDef } from '@mui/x-data-grid'
-import { AcceptanceProduct } from './../../../types/acceptance.types'
 
-import { IProduct, IProductSpecs } from '../../../features/order/order.types'
 import {
   Acceptance,
   AcceptanceProductSpecification,
+  Label,
   StaffMember
 } from '../../../types/acceptance.types'
+import { AcceptanceProduct } from './../../../types/acceptance.types'
 
 export type Row = Pick<
   Acceptance,
@@ -19,11 +19,27 @@ export type AcceptanceProductColumn = GridColDef & {
   field: keyof AcceptanceProduct | keyof AcceptanceProductSpecification
 }
 
-export type AcceptanceProductRow = Omit<IProductSpecs, 'product' | 'cny_to_rub_course'> &
-  Omit<IProduct, 'photo_id' | 'url' | 'category' | 'color'>
+export type AcceptanceProductRow = Omit<AcceptanceProductSpecification, 'product'> &
+  Omit<
+    AcceptanceProduct,
+    'photo_id' | 'url' | 'category' | 'color' | 'last_cost' | 'linked_china_product_size'
+  >
 
 export type StaffMemberFields = Pick<StaffMember, 'username' | 'password'>
 
-export type RussianProductColumn = GridColDef & { field: keyof AcceptanceProduct }
+export type RussianProductColumn = GridColDef & {
+  field: keyof AcceptanceProduct | 'update' | 'delete'
+}
 
-export type RussianProductRow = Pick<AcceptanceProduct, 'id' | 'article' | 'brand' | 'color'>
+export type RussianProductRow = AcceptanceProduct
+
+export type LabelRow = Label & Pick<AcceptanceProduct, 'photo'>
+
+export type ValidatedLabel = Label
+
+export type ValidatedProduct = Omit<ValidatedLabel, 'quantity'>
+
+export type LabelResponse = {
+    status: string;
+    url: string;
+} | undefined
--- src/types/acceptance.types.ts
+++ src/types/acceptance.types.ts
@@ -1,4 +1,4 @@
-import { IProductSpecs } from '../features/order/order.types'
+import { CommonProduct } from '../components/common/common.types'
 import { Modify } from '../utils/types'
 
 export type Acceptance = {
@@ -11,7 +11,7 @@ export type Acceptance = {
   shipped_from_china: string
   custom_id: string | null
   created_at: string
-  products: IProductSpecs[]
+  products: AcceptanceProductSpecification[]
 }
 
 type ModifiedOrder = Modify<
@@ -46,21 +46,53 @@ export type CreateStaffMember = Omit<StaffMember, 'id'>
 //*************************
 
 export type AcceptanceProduct = {
-  id: number
-  title: string
-  photo?: string
-  photo_id?: string
   last_cost: number
-  category?: string
-  article: string
-  size: string
-  brand: string
-  color: string
-}
+  barcode?: string
+  wb_article?: string
+  total_left?: number
+  linked_china_product_article?: string
+  linked_china_product_size?: string
+} & CommonProduct
+
+export type CreateAcceptanceProduct = Modify<
+  Omit<AcceptanceProduct, 'linked_china_product_size' | 'photo_id' | 'id'>,
+  { category?: number; photo?: number; last_cost?: number }
+>
+
+export type UpdateAcceptanceProduct = Partial<CreateAcceptanceProduct> & { id: number }
 
 export type AcceptanceProductSpecification = {
   id: number
   product: AcceptanceProduct
   cost: number
   quantity: number
+}
+
+//*************************
+//-------------------------
+//*************************
+
+export type AcceptanceCategory = {
+  id: number
+  category: string
+}
+
+//*************************
+//-------------------------
+//*************************
+
+export type Label = {
+  id: number
+  title: string
+  barcode: string
+  article: string
+  size: string
+  color: string
+  quantity: number
+}
+
+export type CreateLabel = Label & {
+  individual: string
+  composition: string
+  address: string
 }
