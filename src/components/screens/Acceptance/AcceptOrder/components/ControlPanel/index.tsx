56ebcba74891fefc3667d49bda7ef13ba593100c
Aquarell <aquarell.dev@gmail.com>
2022-11-30 12:38:20 +0300

    added controlling buttons

---
+++ src/components/screens/Acceptance/AcceptOrder/components/ControlPanel/index.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { GreenButton, IndigoButton, RedButton } from '../../../../../ui/Button'
+
+const ContorlPanel: FC<{ updateAcceptance: () => Promise<void> }> = ({ updateAcceptance }) => {
+  const navigate = useNavigate()
+
+  return (
+    <div className='flex space-x-4 py-4 items-center border-t border-slate-800'>
+      <IndigoButton
+        type='button'
+        handler={async () => await updateAcceptance()}
+        text='Сохранить'
+        customWidth='w-60'
+      />
+      <GreenButton
+        type='button'
+        handler={async () => {
+          await updateAcceptance()
+          navigate('../../')
+        }}
+        text='Сохранить и Закрыть'
+        customWidth='w-60'
+      />
+      <RedButton
+        type='button'
+        handler={() => {}}
+        text='Удалить Приемку'
+        customWidth='w-60'
+      />
+    </div>
+  )
+}
+
+export default ContorlPanel
