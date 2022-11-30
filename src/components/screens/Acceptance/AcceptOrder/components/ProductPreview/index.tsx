a69f210c0ee9febddf6c85e32de0b12da151f4b7
Aquarell <aquarell.dev@gmail.com>
2022-11-30 12:44:53 +0300

    product preview on the left

--- src/components/screens/Acceptance/AcceptOrder/components/ProductPreview/index.tsx
+++ src/components/screens/Acceptance/AcceptOrder/components/ProductPreview/index.tsx
@@ -1,35 +1,99 @@
-import { FC } from 'react'
+import { FC, Fragment, ReactNode } from 'react'
 import { SpinnerComponent } from 'react-element-spinner'
 
 import { AcceptanceProductSpecification } from '../../../../../../types/acceptance.types'
+import { getFourDigitId } from '../../../../../../utils'
+import { AbsoluteCenteredContainer } from '../../../../../ui/Container'
+import { Method } from '../../../types'
 
 type ProductPreviewProps = {
   loading: boolean
   specification: AcceptanceProductSpecification | undefined
+  method: Method
 }
 
-const ProductPreview: FC<ProductPreviewProps> = ({ loading, specification }) => {
+const SpecificationProperty: FC<{ title: string; value: string | undefined }> = ({
+  title,
+  value
+}) => (
+  <p className='text-lg text-ellipsis whitespace-nowrap overflow-x-hidden'>
+    {title}:<br />
+    <span className='font-medium'>{value}</span>
+  </p>
+)
+
+const getProperties = (specification: AcceptanceProductSpecification): ReactNode[] => [
+  <SpecificationProperty
+    title='Артикул Поставщика'
+    value={specification.product.linked_china_product_article}
+  />,
+  <SpecificationProperty
+    title='Приемка'
+    value={getFourDigitId(123)}
+  />,
+  <SpecificationProperty
+    title='Размер'
+    value={specification.product.size}
+  />,
+  <SpecificationProperty
+    title='Цвет'
+    value={specification.product.color}
+  />,
+  <SpecificationProperty
+    title='Бренд'
+    value={specification.product.brand}
+  />
+]
+
+const ProductPreview: FC<ProductPreviewProps> = ({ loading, specification, method }) => {
   return (
-    <>
-      <div className='flex space-x-4 items-start border w-full border-slate-800 rounded-sm p-4'>
-        {loading && (
-          <SpinnerComponent
-            loading
-            position='centered'
-            backgroundColor='#9ca3af'
-          />
-        )}
-        <img
-          src={specification?.product.photo}
-          alt={specification?.product.title}
-          className='h-full'
+    <div className='relative flex space-x-4 items-start border w-full border-slate-800 rounded-sm p-4 h-[80%]'>
+      {loading && (
+        <SpinnerComponent
+          loading
+          position='centered'
+          backgroundColor='#9ca3af'
         />
-        <div>
-          <p className='text-3xl font-medium'>{specification?.product.title}</p>
-          <p className='text-3xl font-medium'>Штрих-код: {specification?.product.barcode}</p>
-        </div>
-      </div>
-    </>
+      )}
+      {specification ? (
+        <>
+          <div
+            className='flex flex-col space-y-2 h-full'
+            style={{ flex: '0 0 auto' }}
+          >
+            <img
+              src={specification.product.photo}
+              alt={specification.product.title}
+              className='h-[80%]'
+              style={{ flex: '0 0 auto' }}
+            />
+            <div className='p-4 border-b border-r border-slate-600'>
+              <p className='text-2xl font-medium'>{specification.product.title}</p>
+              <p className='text-2xl font-medium'>
+                {method === 'box' ? (
+                  <>Штрих-код: {specification.product.barcode ?? 'Не указан'}</>
+                ) : (
+                  <>Коробки: {specification.boxes.join(', ')}</>
+                )}
+              </p>
+            </div>
+          </div>
+          <div className='flex flex-col space-y-1 text-ellipsis whitespace-nowrap'>
+            {getProperties(specification).map((prop, idx) => (
+              <Fragment key={idx}>{prop}</Fragment>
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          {!loading && (
+            <AbsoluteCenteredContainer>
+              <h2 className='text-4xl'>Нет данных</h2>
+            </AbsoluteCenteredContainer>
+          )}
+        </>
+      )}
+    </div>
   )
 }
 
