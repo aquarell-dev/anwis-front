b75e87cab1e493318700d956fba3b00120037c16
Aquarell <aquarell.dev@gmail.com>
2022-11-30 12:40:52 +0300

    new func

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,16 @@
-export const cn = (...classes: string[]) => classes.filter(Boolean).join(' ');
+export const cn = (...classes: string[]) => classes.filter(Boolean).join(' ')
 
-export const getDateDiff = (biggerDate: Date, smallerDate: Date): number => Math.ceil(
-  (biggerDate.getTime() - smallerDate.getTime()) / (1000 * 3600 * 24)
-);
+export const getDateDiff = (biggerDate: Date, smallerDate: Date): number =>
+  Math.ceil((biggerDate.getTime() - smallerDate.getTime()) / (1000 * 3600 * 24))
 
 export const convertDateToUSFormat = (dateString: string) =>
-  new Date(dateString.replace(/(\d+[/])(\d+[/])/, '$2$1'));
+  new Date(dateString.replace(/(\d+[/])(\d+[/])/, '$2$1'))
 
-export const getFourDigitId = (id?: number, space?: boolean) => id ? `${space ? '№ ' : '№'}` + '0'.repeat(4 - id.toString().length) + id.toString() : '';
+export const getFourDigitId = (id?: number, space?: boolean) =>
+  id ? `${space ? '№ ' : '№'}` + '0'.repeat(4 - id.toString().length) + id.toString() : ''
 
-export const getCurrentDateTime = () => `${new Date().toLocaleTimeString('en-GB')} ${new Date().toLocaleDateString('en-GB')}`;
+export const getCurrentDateTime = () =>
+  `${new Date().toLocaleTimeString('en-GB')} ${new Date().toLocaleDateString('en-GB')}`
+
+export const getOnlySpecificKeys = <T extends object>(obj: T, keys: (keyof T)[]) =>
+  Object.fromEntries(Object.entries(obj).filter(([key]) => !keys.includes(key as keyof T)))
