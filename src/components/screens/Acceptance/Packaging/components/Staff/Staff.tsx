8fc973876f7041876cfb395a52bb24feee2eb366
Aquarell <aquarell.dev@gmail.com>
2022-12-15 20:14:08 +0300

    adaptivity

--- src/components/screens/Acceptance/Packaging/components/Staff/Staff.tsx
+++ src/components/screens/Acceptance/Packaging/components/Staff/Staff.tsx
@@ -7,7 +7,7 @@ import StaffMember from '../StaffMember'
 const Staff: FC<{ members: TStaffMember[]; loading: boolean }> = ({ members, loading }) => {
   return (
     <div className='w-full border-t border-slate-800 py-4 min-h-[120px]'>
-      <div className='w-full grid grid-cols-2 md:grid-cols-3 place-items-center gap-x-4 gap-y-6'>
+      <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 place-items-center gap-x-4 gap-y-6'>
         <SpinnerComponent
           loading={loading}
           position='centered'
