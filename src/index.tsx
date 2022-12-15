5a01793181e99c3f2c9160c5a5231fa37bdcad12
Aquarell <aquarell.dev@gmail.com>
2022-12-15 20:13:58 +0300

    disabled strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import App from './App'
+import './index.css'
+import { store } from './store/store'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App/>
-    </Provider>
-  </React.StrictMode>
-);
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
