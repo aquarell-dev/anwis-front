2c34fd89e6c4518f5cf4227c317752032fa0e607
Aquarell <aquarell.dev@gmail.com>
2022-12-30 16:49:00 +0300

    meta tags, fonts, store, etc...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import React from 'react'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
+import Layout from '../src/components/layouts/Layout'
+import { store } from '../src/store/store'
+import '../styles/globals.css'
+import '../src/components/ui/ImagePreview/preview.css'
+
+import { Montserrat } from '@next/font/google'
+
+const montserrat = Montserrat({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Provider store={store}>
+        <main className={montserrat.className}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </main>
+      </Provider>
+      <ToastContainer
+        bodyClassName='popupException'
+        className='popupException'
+        toastClassName='popupException'
+      />
+    </>
+  )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,48 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/favicon/apple-touch-icon.png'
+        />
+        <link
+          rel='shortcut icon'
+          href='/favicon/favicon.ico'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon/favicon-16x16.png'
+        />
+        <link
+          rel='manifest'
+          href='/favicon/site.webmanifest'
+        />
+        <meta
+          name='msapplication-TileColor'
+          content='#da532c'
+        />
+        <meta
+          name='theme-color'
+          content='#ffffff'
+        />
+        <meta
+          name='description'
+          content='Anwis Sklad Dashboard'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
