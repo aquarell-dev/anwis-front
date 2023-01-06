a4b746f211dd2e0b3ca90e6e19bf5d10c4637ba5
Aquarell <aquarell.dev@gmail.com>
2023-01-06 18:36:24 +0300

    test loader

--- src/components/ui/Loader/index.tsx
+++ src/components/ui/Loader/index.tsx
@@ -1,18 +1,18 @@
 import { FC } from 'react'
-import { PuffLoader } from 'react-spinners'
 
 import { AbsoluteCenteredContainer } from '../Container'
 
 const Loader: FC<{ isLoading?: boolean }> = ({ isLoading }) => {
   return (
     <AbsoluteCenteredContainer>
-      <PuffLoader
+      <p>Loading...</p>
+      {/* <PuffLoader
         color={'#059669'}
         loading={isLoading || false}
         size={100}
         aria-label='Loading Spinner'
         data-testid='loader'
-      />
+      /> */}
     </AbsoluteCenteredContainer>
   )
 }
